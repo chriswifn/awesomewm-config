385a9398a50af43e8aa65b2360b453df3c63a91e
Christian Hageloch <christian.hageloch@gmail.com>
2023-01-08 14:37:02 +0100

    indentation

--- core/keys/terminal.lua
+++ core/keys/terminal.lua
@@ -3,26 +3,26 @@ local awful = require("awful")
 local apps = require("core.apps")
 
 local terminalkeys = gtable.join(
-    -- terminal applications
-    awful.key ( {apps.modkey}, "t", function()
-	    local grabber
-	    grabber =
+-- terminal applications
+	awful.key({ apps.modkey }, "t", function()
+		local grabber
+		grabber =
 		awful.keygrabber.run(
-		    function(_, key, event)
-			if event == "release" then return end
-			if key == "t" then
-			    local matcher = function(c)
-				return awful.rules.match(c, {class = "st-256color"}) 
+			function(_, key, event)
+			    if event == "release" then return end
+			    if key == "t" then
+				local matcher = function(c)
+				    return awful.rules.match(c, { class = "st-256color" })
+				end
+				awful.client.run_or_raise(apps.terminal .. "-e tmux new-session -A -s tempterm", matcher)
+			    elseif key == "n" then awful.spawn(apps.terminal)
 			    end
-			    awful.client.run_or_raise(apps.terminal .. "-e tmux new-session -A -s tempterm", matcher)
-			elseif key == "n" then awful.spawn(apps.terminal)
+			    awful.keygrabber.stop(grabber)
 			end
-			awful.keygrabber.stop(grabber)
-		    end
 		)
-    end,
-	{description = "terminal keys", group="terminal"}
-    )
+	end,
+		{ description = "terminal keys", group = "terminal" }
+	)
 )
 
 return terminalkeys
